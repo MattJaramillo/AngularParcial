de858fcff813bc6655c62d6c978cd3885fae04ae
Mateo Jaramillo <m.jaramillos2@uniandes.edu.co>
2023-11-04 23:13:53 -0500

    HTTP GET

--- angular_parcial/src/app/plantas/plantas.component.ts
+++ angular_parcial/src/app/plantas/plantas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Plantas } from './plantas';
 import { dataPlantas } from './dataPlantas';
+import { PlantasService } from './plantas.service';
 
 @Component({
   selector: 'app-plantas',
@@ -8,6 +9,7 @@ import { dataPlantas } from './dataPlantas';
   styleUrls: ['./plantas.component.css']
 })
 export class PlantasComponent implements OnInit {
+  
   plantas: Array<Plantas> = [];
   constructor() { }
   getPlantasList(): Array<Plantas>{
